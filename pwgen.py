0f0935233c51ea7444df9a39fe66b1c2899a1fb2
emma <emma.e.glenna@gmail.com>
2023-07-03 15:46:22 +0200

    added more error handling

--- pwgen.py
+++ pwgen.py
@@ -7,22 +7,6 @@ import re
 #import requests
 from urllib.request import urlopen
 
-parser = argparse.ArgumentParser(
-                    prog='pwgen',
-                    description='Generates a secure password')
-parser.add_argument('-u', '--uppercase', action='store_true')   # Uppercase
-parser.add_argument('-d', '--digits', action='store_true')      # Digits
-parser.add_argument('-s', '--symbols', action='store_true')     # symbols 
-parser.add_argument('-l', '--length')                           # length
-parser.add_argument('-w', '--words', action='store_true')       # dictionary words
-args = parser.parse_args()
-
-pw_length = 16
-words_amount = 4
-if(args.length != None):
-    pw_length = int(args.length)
-    if args.words:
-        words_amount = int(args.length)
 
 def fetch_words(word_list=[]):
     url = "https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt"
@@ -38,17 +22,28 @@ def generate():
     digits      = ''.join(secrets.choice(string.digits)             for i in range(64))
     symbols     = ''.join(secrets.choice(string.punctuation)        for i in range(64))
     combined_sequence = None
-    word_list = fetch_words()
-    words = [secrets.choice(word_list) for x in range(words_amount)]
+    words = [""]
+    if args.words:
+        word_list = fetch_words()
+        words = [secrets.choice(word_list) for x in range(words_amount)]
     
     if(args.uppercase):
-        combined_sequence = uppercase
+        try:
+            combined_sequence += uppercase
+        except TypeError:
+            combined_sequence = uppercase
         words = [i.capitalize() for i in words]
     if(args.digits):
-        combined_sequence += digits
+        try:
+            combined_sequence += digits
+        except TypeError:
+            combined_sequence = digits
         words.insert(0, digits[0:2])
     if args.symbols:
-        combined_sequence += symbols 
+        try:
+            combined_sequence += symbols
+        except TypeError:
+            combined_sequence = symbols
         words.append(symbols[0:2])
     if args.words:
         password = '-'.join(words[0:words_amount])
@@ -63,4 +58,23 @@ def generate():
         password = ''.join(secrets.choice(combined_sequence) for i in range(pw_length))
     return password
 
-print(generate())
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                        prog='pwgen',
+                        description='Generates a secure password')
+    parser.add_argument('-u', '--uppercase', action='store_true')   # Uppercase
+    parser.add_argument('-d', '--digits', action='store_true')      # Digits
+    parser.add_argument('-s', '--symbols', action='store_true')     # symbols 
+    parser.add_argument('-l', '--length')                           # length
+    parser.add_argument('-w', '--words', action='store_true')       # dictionary words
+    args = parser.parse_args()
+
+    pw_length = 16
+    words_amount = 4
+    
+    if(args.length != None):
+        pw_length = int(args.length)
+        if args.words:
+            words_amount = int(args.length)
+
+    print(generate())
