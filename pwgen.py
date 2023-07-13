f4d4940a6ab8d3fd79add6cedbaeca8d7a3e8535
cityjeans <emma@eilefsen.xyz>
2023-07-14 01:17:24 +0200

    refactored and added error handling to fetch_words(), thus making it a universal function to fetch words regardless if there is a file or not

--- pwgen.py
+++ pwgen.py
@@ -3,23 +3,21 @@ import string
 import secrets
 import argparse
 
-#import requests
+#import request
 from urllib.request import urlopen
 
-def fetch_local_words():
+def fetch_words():
     word_list = [""]
-    with open("eff_words.txt", "r") as file:
-        for line in file:
-            word_list.append(line.rstrip())
-    return word_list
-
-def fetch_url_words():
-    word_list = [""]
-    url = "https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt"
-    with urlopen(url) as file:
-        for line in file:
-            line_parse = line.split()
-            word_list.append(line_parse[1].decode('utf-8'))
+    try:
+        with open("eff_words.txt", "r") as file:
+            for line in file:
+                word_list.append(line.rstrip())
+    except FileNotFoundError:
+        url = "https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt"
+        with urlopen(url) as file:
+            for line in file:
+                line_parse = line.split()
+                word_list.append(line_parse[1].decode('utf-8'))
     return word_list
 
 def generate(word_list):
@@ -29,9 +27,9 @@ def generate(word_list):
     symbols     = ''.join(secrets.choice('/=!@#$%&*()')        for i in range(64))
     combined_sequence = ""
     words = [""]
+
     if args.words:
         words = [secrets.choice(word_list) for x in range(words_amount)]
-
     if args.uppercase:
         combined_sequence += uppercase
         words = [i.capitalize() for i in words]
@@ -45,28 +43,27 @@ def generate(word_list):
         password = '-'.join(words)
     else:
         combined_sequence += lowercase
-        password = ''.join(secrets.choice(combined_sequence) for i in range(pw_length))
+        password = ''.join(secrets.choice(combined_sequence) for i in range(int(args.length)))
+
     return password
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
                         prog='pwgen',
                         description='Generates a secure password')
-    parser.add_argument('-u', '--uppercase', action='store_true')   # Uppercase
-    parser.add_argument('-d', '--digits', action='store_true')      # Digits
-    parser.add_argument('-s', '--symbols', action='store_true')     # symbols 
-    parser.add_argument('-l', '--length')                           # length
-    parser.add_argument('-w', '--words', action='store_true')       # dictionary words
+    parser.add_argument('-u', '--uppercase', action='store_true', help='use uppercase letters in the password')   # Uppercase
+    parser.add_argument('-d', '--digits', action='store_true', help='use digits (numbers 0 to 9) in the password')      # Digits
+    parser.add_argument('-s', '--symbols', action='store_true', help='use special symbols in the password')     # symbols 
+    parser.add_argument('-w', '--words', action='store_true', help='use english words in the password')       # dictionary words
+    parser.add_argument('-l', '--length', default=16, help="set the length of the password.")                           # length
     args = parser.parse_args()
 
-    pw_length = 16
     words_amount = 4
-
-    if args.length is not None:
-        pw_length = int(args.length)
-        if args.words:
+    eff_word_list = [""]
+    if args.words:
+        eff_word_list = fetch_words()
+        if args.length != 16:
             words_amount = int(args.length)
 
-eff_word_list = fetch_local_words()
 for i in range(25):
     print(generate(eff_word_list))
